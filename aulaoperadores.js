944edca8ff8b84b24b21ebb69c906b5c8f5245cd
Marcelo <marcelotmsg@gmail.com>
2020-06-16 20:14:09 -0300

    aulaoperadores.js adicionado

---
+++ aulaoperadores.js
@@ -0,0 +1,10 @@
+const name = 'Marcelo'
+const idade = 17
+
+if (idade >= 18) {
+    console.log(`Olá ${name} pode entrar! Pois você tem ${idade} anos!`)
+} else if (idade === 17) {
+    console.log(`${name}! Cara você tem ${idade} anos! Aguenta mais um pouco manolo!!!!!`)
+} else {
+    console.log(`${name}! Você tem apenas ${idade} anos! Caia fora daqui seu pivete leite com pera!!!`)
+}
